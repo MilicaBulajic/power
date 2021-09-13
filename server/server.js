bb6c9b38d03aed4be2268896cef27312e3839d74
Milica Bulajic <milica.bulajic@gmail.com>
2021-09-13 21:49:46 +0200

    solved sequelize error - deleted wrong tables and running the migration command

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const userRouter = require("./routes/users");
+const projectRouter = require("./routes/projects");
 const server = express();
 const path = __dirname + '/views/';
 server.use(express.static(path));
@@ -9,18 +10,13 @@ server.use(express.static(path));
 server.use(morgan("dev"));
 server.use(cors({ origin: true }));
 
-server.use(userRouter);
+server.use('/login', userRouter);
+server.use("/project", projectRouter);
 
 server.get("/", (req, res) => {
   res.send({ message: "Hello, connection succeeded" });
 });
 
-server.use((req, res, next) => {
-  const err = new Error("The requested resource couldn't be found.");
-  err.status = 404;
-  err.errors = ["Could not find string of resource"];
-  next(err);
-});
 
 
 
