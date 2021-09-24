2477de08a37d708b9f93598aaf4a5b193665ede2
Milica Bulajic <milica.bulajic@gmail.com>
2021-09-24 17:28:56 +0200

    developing drawer for the logged-in users

--- client/src/components/PrivateRoutes.js
+++ client/src/components/PrivateRoutes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Dasboard from "./Landing/Dasboard";
-
+import CustomDrawer from "./Navigation/CustomDrawer"
 
 const PrivateRoutes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Dasboard} />
+                <CustomDrawer>
+                     <Route exact path="/" component={Dasboard} />
+                </CustomDrawer>
             </Switch> 
         </BrowserRouter> 
     )
